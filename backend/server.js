2d5466e7507102b51445c03d0f12ec7f1893d03e
Francisco Alfaro <Francisco Alfaro>
2025-02-07 15:31:39 -0800

    Deploy backend to Render

--- backend/server.js
+++ backend/server.js
@@ -83,13 +83,32 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
+//before and after
+/**const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+}); */
+
+//now removed??
+/**const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render PostgreSQL
+  },
+}); */
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: false, // Disable SSL for local connection
 });
+
 //try
 
 console.log("DB Password:", typeof process.env.DB_PASSWORD);
